cef1dde43b8b193e2452ea340e5e42a88b1c79c3
Cucurrucho <adam.bar91@gmail.com>
2023-03-21 17:02:06 -0300

    Map and auth added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,29 @@
-import './bootstrap'
-import { createApp } from 'vue'
-import HelloWorld from './Components/HelloWorld.vue'
+import './bootstrap';
+import '../css/app.css';
 
-window.app = createApp({
-    setup() {
-        return {
-            message: 'Welcome to Your Vue.js App',
-        };
+import {createApp, h} from 'vue';
+import {createInertiaApp} from '@inertiajs/vue3';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
+import {ZiggyVue} from '../../vendor/tightenco/ziggy/dist/vue.m';
+import VueGoogleMaps from '@fawmi/vue-google-maps'
+
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({el, App, props, plugin}) {
+        return createApp({render: () => h(App, props)})
+            .use(plugin)
+            .use(ZiggyVue, Ziggy)
+            .use(VueGoogleMaps, {
+                load: {
+                    key: 'AIzaSyCYv-72Jwz1NIrWHmomWZsjsmoMSPdNnGs',
+                },
+            })
+            .mount(el);
     },
-    components: {
-        HelloWorld
+    progress: {
+        color: '#4B5563',
     },
-}).mount('#app');
+});
