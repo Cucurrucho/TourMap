7b942e5bc7e750a550b33e812ca4e403be12e487
Cucurrucho <adam.bar91@gmail.com>
2023-06-14 16:17:07 -0300

    added more info to manifest

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,7 +14,7 @@
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
-        <link rel="manifest" href="mainfest.json">
+        <link rel="manifest" href="/mainfest.json">
 
     </head>
     <body class="font-sans antialiased">
